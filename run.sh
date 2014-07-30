084f12ca920f6fd57a9083bef089de3686dd8c27
William Hanwoody <hanwoody@gmail.com>
2014-07-30 18:13:40 +0800

    fix test

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ envfile=${CONF_DIR-:/opt/nicedocker/conf}/env
 . $envfile &&
 eval `cat $envfile|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
 
-if [ -z "$MAIL_DOMAIN" -o -z "$DP_USER" -o "$DP_PASS" ]; then
+if [ -z "$MAIL_DOMAIN" -o -z "$DP_USER" -o -z "$DP_PASS" ]; then
   echo "you must supply env MAIL_DOMAIN|DP_USER|DP_PASS"
   exit 1
 fi
