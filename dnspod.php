1da3366fa32fae83fbcca8860d784277a0cbabc5
William Hanwoody <hanwoody@gmail.com>
2014-07-30 18:12:58 +0800

    ENV should be CAPITAL

--- dnspod.php
+++ dnspod.php
@@ -1,9 +1,9 @@
 #!/usr/bin/env php
 
 <?php
-$mail_domain = getenv("mail_domain");
-$dp_user = getenv("dp_user");
-$dp_pass = getenv("dp_pass");
+$mail_domain = getenv("MAIL_DOMAIN");
+$dp_user = getenv("DP_USER");
+$dp_pass = getenv("DP_USER");
 $interval = getenv("CHECK_INTERVAL");
 if($interval == null) $interval = 550;
 
@@ -56,7 +56,7 @@ function pdie($msg) {
   sleep($interval);
 }
 
-echo "check domain list...\n";
+echo "check domain list ...";
 $domain_list_url = 'https://dnsapi.cn/Domain.List';
 $data_domlist = array (
 'login_email'=>$dp_user,
@@ -74,9 +74,14 @@ foreach ($domlist_arr["domains"] as $d) {
     break;
   }
 }
-$domain_id || die("domain $base_domain not create in your dnspod.");
+if ($domain_id) {
+  echo " OK.\n";
+} else {
+  echo " NO.\n";
+  die("domain $base_domain not create in your dnspod.");
+}
 
-echo "check record list...\n";
+echo "check record list ... ";
 $record_list_url = 'https://dnsapi.cn/Record.List';
 $data_reclist = array (
 'login_email'=>$dp_user,
@@ -88,6 +93,8 @@ $reclist || die ("failed to get record list for domain $base_domain");
 $reclist_arr = json_decode($reclist, true);
 if (!$reclist_arr or $reclist_arr['status']['code'] != 1)
   die("error when parse the record list return value.");
+
+echo "OK\n";
 
 $record_id = null;
 $record_value = null;
@@ -106,7 +113,8 @@ echo $localip . "\n";
 $txtvalue = "v=spf1 ip4:$localip -all";
 
 if (!$record_id) {
-  echo "create record $sub_domain...\n";
+  echo "record $sub_domain type txt not existed yet.\n";
+  echo "create record $sub_domain type txt ... ";
   $record_create_url = 'https://dnsapi.cn/Record.Create';
   $data_record_create = array (
   'login_email'=>$dp_user,
@@ -122,19 +130,21 @@ if (!$record_id) {
   $rcr_arr = json_decode($rc_result, true);
   if (!$rcr_arr or $rcr_arr['status']['code'] != 1)
     die ("error when parse the record create return value");
+  echo "OK\n";
   $record_id = $rcr_arr['record']['id'];
+  echo "you can run 'dig txt $mail_domain' to verify in linux shell.";
 } 
 
 
 echo "begin while loop ...\n";
 while ( true ) {
   sleep($interval);
-  echo "get public ip ...";
+  echo "get public ip ... ";
   $localip = get_ip();
   $localip || pdie("failed to get your public ip.");
   $txtvalue = "v=spf1 ip4:$localip -all";
   echo $localip . "\n";
-  echo "check record info for domain $mail_domain...\n"; 
+  echo "check record info for domain $mail_domain ... "; 
   $record_info_url = "https://dnsapi.cn/Record.Info";
   $data_record_info = array (
   'login_email'=>$dp_user,
@@ -156,7 +166,7 @@ while ( true ) {
     continue;
   }
 
-  echo "public ip changed, should change dns to $localip...\n";
+  echo "public ip changed, should change dns to $localip ... ";
   $record_modify_url = "https://dnsapi.cn/Record.Modify";
   $data_record_modify = array (
   'login_email'=>$dp_user,
@@ -174,4 +184,5 @@ while ( true ) {
   $rmr_arr = json_decode($rm_result, true);
   if (!$rmr_arr or $rmr_arr['status']['code'] != 1)
     pdie ("error when parse the record modify return value");
+  echo "OK\n";
 }
